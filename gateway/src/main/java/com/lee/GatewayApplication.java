5fb3fec6032da6b8a1a0cca495b57d426203caff
liguoyu <liguoyu@huacun.com>
2021-01-29 11:26:40 +0800

    first commit

---
+++ gateway/src/main/java/com/lee/GatewayApplication.java
@@ -0,0 +1,25 @@
+package com.lee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lee
+ * @version 1.0
+ * @date 2021/1/28
+ */
+
+@RestController
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class);
+    }
+
+    @RequestMapping("/he")
+    public String m(){
+        return "123";
+    }
+}
